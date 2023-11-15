60d305bce71eb62f62402b86e21328d9f4d3367c
Konrad Kubacki <konradkg@o2.pl>
2023-11-15 06:54:48 +0100

    empty spring boot template

---
+++ src/main/java/dev/promoclock/PromoClockApplication.java
@@ -0,0 +1,13 @@
+package dev.promoclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoClockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/promoclock/PromoClockApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.promoclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoClockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
